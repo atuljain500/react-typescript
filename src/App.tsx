a2df31336fdd2a6eebb4fd30ef445aa8583fef40
Atul <atuljain.tech@gmail.com>
2025-08-17 13:34:11 +0530

    added changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,26 @@ import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [data, setData] = useState([10, 20, 30, 40, 60]);
   const incrementHandler = () => {
     setCount(count => count + 1);
+  }
+  const onClickItem = (event: any) => {
+    console.log(event.target.id);
   }
   return (
     <div className="App">
       <h1>Hello Word</h1>
       <p>{count}</p>
+      <ul onClick={(event) => onClickItem(event)}>
+        {
+          data.map(item => {
+            return (
+              <li style={{listStyle: 'none'}}>{item}</li>
+            )
+          })
+        }
+      </ul>
       <button onClick={incrementHandler}>Click me</button>
       <span>hi</span>
     </div>

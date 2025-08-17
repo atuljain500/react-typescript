932d9c3fb1f163abc033ed97171c38ed1a7099c7
Atul <atuljain.tech@gmail.com>
2025-08-17 13:00:42 +0530

    react typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const incrementHandler = () => {
+    setCount(count => count + 1);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Word</h1>
+      <p>{count}</p>
+      <button onClick={incrementHandler}>Click me</button>
+      <span>hi</span>
     </div>
   );
 }
